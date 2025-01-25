b0530bfff8f267e804fdf5d3183fd72ba5460204
poornima2246 <poornima220500@gmail.com>
2025-01-25 20:33:31 +0530

    first commit

---
+++ Models/CakeModel.js
@@ -0,0 +1,21 @@
+ 
+import mongoose from "mongoose";
+
+const CakeSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    mainImage: { type: String, required: true }, // Main image filename
+    addImage1:{type: String, required: true},
+    addImage2:{type: String, required: true},
+    category: { type: String, required: true },
+
+      // additionalImages: [{ type: String }], // Array of additional image filenames
+});
+
+const CakeModel = mongoose.models.cake || mongoose.model("cake", CakeSchema);
+
+export default CakeModel;
+
+
+ 
